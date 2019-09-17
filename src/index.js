e925092d57b81365ffac19f59c974a3b44ebd0ca
Eric Meldrum <eric.meldrum@gmail.com>
2019-09-17 10:15:02 -0600

    Change layout from 4 tabs to 2 tabs

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
 import $ from 'jquery';
 import './css/base.scss';
 import Hotel from './Hotel.js';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import DOMupdates from './DOMupdates.js'
 import './images/bamboo-background.jpg'
 
 let hotel;
 
-let roomServiceData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices')
-  .then(data => data.json());
-
-let userData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
-  .then(data => data.json());
-
-let roomData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
-  .then(data => data.json());
-
-let bookingData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
-  .then(data => data.json());
-  
-Promise.all([roomServiceData, userData, roomData, bookingData])
-  .then(hotel = new Hotel(userData, roomData, bookingData, roomServiceData))
-  .catch(function (err) {
-    console.log('Unable to fetch the data', err)
-  });
+Promise.all([
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+    .then(data => data.json()), 
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+    .then(data => data.json()), 
+  fetch ('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')  
+    .then(data => data.json()), 
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices')
+    .then(data => data.json())])
+    .then(data => hotel = new Hotel(data[0], data[1], data[2], data[3]))
+    .then(() => populateDOM());
 
   // Show the first tab by default
 $('.tabs-stage div').hide();
@@ -37,3 +31,21 @@ $('.tabs-nav a').on('click', function(event){
   $('.tabs-stage div').hide();
   $($(this).attr('href')).show();
 });
+
+function populateDOM() {
+  let date = '2019/09/18';
+  let numberAvailable = hotel.getRoomsAvailable(date);
+  let occupancy = hotel.getOccupancy(date);
+  let totalRevenue = hotel.getTotalRevenue(date);
+  let mostPopularDates = hotel.findMostPopularDates();
+  let leastPopularDates = hotel.findLeastPopularDates();
+
+
+  DOMupdates.displayDate(date);
+  DOMupdates.displayRoomsAvail(numberAvailable);
+  DOMupdates.displayOccupancy(occupancy);
+  DOMupdates.displayTotalRevenue(totalRevenue);
+  DOMupdates.displayMostPopularDates(mostPopularDates);
+  DOMupdates.displayLeastPopularDates(leastPopularDates);
+
+}
