c9cf7c95046827faef42b83044b098544d00cae2
Eric Meldrum <eric.meldrum@gmail.com>
2019-09-17 10:13:29 -0600

    Append all initial information to dashboard

--- src/DOMupdates.js
+++ src/DOMupdates.js
@@ -1,2 +1,40 @@
 import $ from 'jquery';
 
+const DOMupdates = {
+  displayDate(date) {
+    $('#todays-date').text(date);
+  },
+
+  displayRoomsAvail(number) {
+    $('#rooms-avail').text(number)
+  },
+
+  displayOccupancy(number) {
+    $('#occupancy').text(`${number}% of rooms are occupied on this date.`)
+  },
+
+  displayTotalRevenue(number) {
+    $('#total-revenue').text(`$${number}`)
+  },
+
+  displayMostPopularDates(array) {
+    array.forEach(date => {
+      $('#most-popular').append(`<li>${date}</li>`)
+    })
+  },
+
+  displayLeastPopularDates(array) {
+    array.forEach(date => {
+      $('#least-popular').append(`<li>${date}</li>`)
+    })
+  },
+
+
+
+
+
+
+
+}
+
+export default DOMupdates;
