d3123d1112c1f2b4cf9c88b8621c6eb4f47875c3
Eric Meldrum <eric.meldrum@gmail.com>
2019-09-17 14:33:45 -0600

    Create populateDates and chooseGuest methods and call them on promise resolve after data has been fetched

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import DOMupdates from './DOMupdates.js'
 import './images/bamboo-background.jpg'
 
 let hotel;
+let today = `${new Date().getFullYear()}/${String(new Date()
+    .getMonth() + 1)
+    .padStart(2, '0')}/${String(new Date()
+    .getDate())
+    .padStart(2, '0')}`;
 
 Promise.all([
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
@@ -16,7 +21,9 @@ Promise.all([
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices')
     .then(data => data.json())])
     .then(data => hotel = new Hotel(data[0], data[1], data[2], data[3]))
-    .then(() => populateDOM());
+    .then(() => populateDOM())
+    .then(() => populateDates())
+    .then(() => populateGuestList());
 
   // Show the first tab by default
 $('.tabs-stage div').hide();
@@ -33,19 +40,48 @@ $('.tabs-nav a').on('click', function(event){
 });
 
 function populateDOM() {
-  let date = '2019/09/18';
-  let numberAvailable = hotel.getRoomsAvailable(date);
-  let occupancy = hotel.getOccupancy(date);
-  let totalRevenue = hotel.getTotalRevenue(date);
+  let numberAvailable = hotel.getRoomsAvailable(today);
+  let occupancy = hotel.getOccupancy(today);
+  let totalRevenue = hotel.getTotalRevenue(today);
   let mostPopularDates = hotel.findMostPopularDates();
   let leastPopularDates = hotel.findLeastPopularDates();
 
-
-  DOMupdates.displayDate(date);
+  DOMupdates.displayCurrentGuest(hotel.currentGuest.name);
+  DOMupdates.displayDate(today);
   DOMupdates.displayRoomsAvail(numberAvailable);
   DOMupdates.displayOccupancy(occupancy);
   DOMupdates.displayTotalRevenue(totalRevenue);
   DOMupdates.displayMostPopularDates(mostPopularDates);
   DOMupdates.displayLeastPopularDates(leastPopularDates);
+}
 
+function populateGuestList() {
+  hotel.guests
+    .sort((guestA, guestB) => (guestA.name > guestB.name ? 1 : -1))
+    .forEach(guest => {
+      DOMupdates.populateGuests(guest.name, guest.id);
+    });
+}
+
+function populateDates() {
+  let dates = hotel.findDatesArray();
+  dates
+  .sort((dateA, dateB) => (dateA > dateB ? 1 : -1))
+  .forEach(date => DOMupdates.populateDatesList(date));
+}
+
+$('#new-guest').on('click', function() {$('#new-guest-form').show()
+});
+
+$('#select-guest-btn').on('click', chooseGuest);
+
+function chooseGuest() {
+  let guest = $('#guest-list').val();
+  hotel.findCurrentGuestInfo(guest);
+  DOMupdates.displayCurrentGuest(guest);
+}
+
+
+function searchCustomer() {
+  hotel.guests.filter(guest => guest.name.includes($('#search-customer').val()));
 }
